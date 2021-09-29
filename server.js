570a77a107e7a30942a184b7ad65df8806d2ba8a
JohanK <klausenjohan23@gmail.com>
2021-09-29 12:50:25 +0200

    connected to html to server

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 'use strict'
 const express = require('express');
+const fs = require ('fs');
 
 const app = express();
 const port = 8042;
 
-app.get('/', (req, res) => {
-    
-    res.send('Hello');
+app.get('/johan', (req, res) => {
+    fs.readFile('app.html', (err, data) => {
+        res.statuscode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Content-Length', data.length);
+        res.write(data);
+        res.end();
+    })
+   
+})
 
+
+app.get('/', (req, res) => {
+    res.send('hejsan');
 })
 
 app.listen(port,() => {
