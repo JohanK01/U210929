f505666fe45ba4cf2da68f449a49c480e7398fd2
JohanK <klausenjohan23@gmail.com>
2021-09-29 13:33:00 +0200

    debugging for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require ('fs');
 const app = express();
 const PORT = process.env.PORT || 8042;
 
-app.get('/app.html', (req, res) => {
+app.get('/', (req, res) => {
     fs.readFile('app.html', (err, data) => {
         res.setHeader('Content-Type', 'text/html');
         res.setHeader('Content-Length', data.length);
