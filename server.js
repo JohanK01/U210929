8c2e3880fd7ca61a75cb305f39ba61feb5190dde
JohanK <klausenjohan23@gmail.com>
2021-09-29 13:06:23 +0200

    connected server to another js file

--- server.js
+++ server.js
@@ -5,20 +5,24 @@ const fs = require ('fs');
 const app = express();
 const port = 8042;
 
-app.get('/johan', (req, res) => {
+app.get('/app.html', (req, res) => {
     fs.readFile('app.html', (err, data) => {
-        res.statuscode = 200;
         res.setHeader('Content-Type', 'text/html');
         res.setHeader('Content-Length', data.length);
         res.write(data);
-        res.end();
+   
     })
    
 })
 
 
-app.get('/', (req, res) => {
-    res.send('hejsan');
+app.get('/exercuse2.js', (req, res) => {
+    fs.readFile('exercuse2.js', (err, data) => {
+        res.setHeader('Content-Type', 'text/javasript');
+        res.setHeader('Content-Length', data.length);
+        res.write(data);
+
+    })
 })
 
 app.listen(port,() => {
