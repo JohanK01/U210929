3884d131d3e6c690f6f6f91f3531d2dae10f4a3e
JohanK <klausenjohan23@gmail.com>
2021-09-29 13:16:56 +0200

    fixing bugs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/app.html', (req, res) => {
     fs.readFile('app.html', (err, data) => {
         res.setHeader('Content-Type', 'text/html');
         res.setHeader('Content-Length', data.length);
-        res.write(data);
+        res.send(data);
    
     })
    
@@ -20,7 +20,7 @@ app.get('/exercuse2.js', (req, res) => {
     fs.readFile('exercuse2.js', (err, data) => {
         res.setHeader('Content-Type', 'text/javasript');
         res.setHeader('Content-Length', data.length);
-        res.write(data);
+        res.send(data);
 
     })
 })
